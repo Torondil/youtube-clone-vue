9eb99a31b406c9507029e4c857fea4132f478f8b
torondil <bulavskipavel@gmail.com>
2023-06-24 13:41:37 +0200

    feat: add base structure and start decomposition

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <TheHeader />
+    <TheSidebarHeader />
+    <TheSidebarSmall />
+    <TheSidebar />
+    <TheSidebarMobile />
+    <TheCategories />
+    <TheVideos />
+  </div>
+</template> 
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+import TheCategories from './components/TheCategories.vue';
+import TheSidebar from './components/TheSidebar.vue';
+import TheSidebarMobile from './components/TheSidebarMobile.vue';
+import TheSidebarSmall from './components/TheSidebarSmall.vue';
+import TheVideos from './components/TheVideos.vue';
+
+export default {
+  components: {
+    TheHeader,
+    TheCategories,
+    TheSidebar,
+    TheSidebarMobile,
+    TheSidebarSmall,
+    TheVideos
+  },
+}
+</script>
